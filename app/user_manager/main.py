5a3ca16485582f227cfc09dfe33f5fb39bf44cb4
faerlin <pulido.faerlin@gmail.com>
2025-07-19 23:40:53 -0400

    system design image

--- app/user_manager/main.py
+++ app/user_manager/main.py
@@ -4,7 +4,7 @@ import app.common.logger.slog  # noqa: F401
 from app.user_manager.handlers.context import lifespan
 from app.user_manager.handlers.exception import general_exception_handler
 from app.user_manager.handlers.middleware import PreProcessRequest
-from app.user_manager.handlers.routes import router
+from app.user_manager.handlers.route import router
 
 server = FastAPI(lifespan=lifespan)
 
